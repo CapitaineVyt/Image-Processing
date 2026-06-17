fec761d928a1b39fa09d2fe66bcff057a1299ae9
CapitaineVyt <iliannpy@gmail.com>
2026-06-17 17:47:32 +0200

    rien

--- src/bmp/t_bmp8.c
+++ src/bmp/t_bmp8.c
@@ -108,8 +108,6 @@ void bmp8_saveImage(const char *filename, t_bmp8 *img){
 
 }
 
-#include <stdio.h>
-#include <string.h>
 
 void bmp8_saveWithFilterName(const char *original_path, const char *filter_name, t_bmp8 *img) {
     if (img == NULL) {
